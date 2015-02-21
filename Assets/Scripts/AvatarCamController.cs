c8006250e6cfb165f9bca7c2147e46f1e60337d6
YuKejun <yukejun@umich.edu>
2015-02-21 14:24:21 -0500

    avatar

---
+++ Assets/Scripts/AvatarCamController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarCamController : MonoBehaviour {
+
+	public float bird_height;
+	public float bird_angle;
+	public float switch_time;  // the time period to complete view switching
+
+	private bool is_switching;
+	private bool is_at_bird; // whether the camera is at birds'-eye-view
+	private Vector3 dest_pos;
+	private Quaternion dest_rotate;   // the target position and rotation of the camera during switching
+
+	// Use this for initialization
+	void Start () {
+		is_at_bird = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// do transition
+		if (is_switching) {
+			// if switching is completed
+			if (transform.rotation.x == dest_rotate.x) {
+				is_switching = false;
+				is_at_bird = !is_at_bird;
+				return;
+			}
+
+			// transit to target camera position
+			transform.position = Vector3.MoveTowards(transform.position, dest_pos, bird_height / switch_time * Time.deltaTime);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, dest_rotate, bird_angle / switch_time * Time.deltaTime);
+			return;
+		}
+
+		// triggers
+		else if (Input.GetKeyDown("3")) {
+			if (is_at_bird) {
+				dest_pos = transform.position - new Vector3(0, bird_height, 0);
+				dest_rotate = transform.rotation * Quaternion.Euler (-bird_angle, 0, 0);
+			}
+			else {
+				dest_pos = transform.position + new Vector3(0, bird_height, 0);
+				dest_rotate = transform.rotation * Quaternion.Euler (bird_angle, 0, 0);
+			}
+			is_switching = true;
+		}
+	}
+}
---
+++ Assets/Scripts/AvatarController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarController : MonoBehaviour {
+
+	public float grid_size;
+	public float walking_speed;
+	public float rotating_speed;
+
+	public Camera player_camera;
+	public Camera main_camera;
+
+	private bool is_walking;
+	private Vector3 walking_dest;
+
+	private bool is_rotating;
+	private Quaternion rotate_dest;
+
+	// Use this for initialization
+	void Start () {
+		is_walking = false;
+		player_camera.enabled = true;
+		main_camera.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (is_walking) {
+			if (transform.position == walking_dest) {
+				is_walking = false;
+				return;
+			}
+			transform.position = Vector3.MoveTowards(transform.position, walking_dest, walking_speed * Time.deltaTime);
+			return;
+		}
+		if (is_rotating) {
+			if (transform.rotation == rotate_dest) {
+				is_rotating = false;
+				return;
+			}
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, rotate_dest, rotating_speed * Time.deltaTime);
+			return;
+		}
+
+		if (!is_walking && Input.GetKey ("space")) {
+			is_walking = true;
+			walking_dest = transform.position + transform.rotation * Vector3.right * grid_size;
+			return;
+		}
+
+		if (!is_rotating && Input.GetKeyDown (KeyCode.RightArrow)) {
+			is_rotating = true;
+			rotate_dest = transform.rotation * Quaternion.Euler (0, 90, 0);
+			return;
+		}
+		if (!is_rotating && Input.GetKeyDown (KeyCode.LeftArrow)) {
+			is_rotating = true;
+			rotate_dest = transform.rotation * Quaternion.Euler (0, -90, 0);
+			return;
+		}
+
+		if (Input.GetKeyDown ("1")) {
+			player_camera.enabled = false;
+			main_camera.enabled = true;
+		} else if (Input.GetKeyDown ("2")) {
+			main_camera.enabled = false;
+			player_camera.enabled = true;
+		}
+	}
+}
