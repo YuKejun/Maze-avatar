6ed559dc82ed5ae59f9c15da1622521dc523953e
luo19930414 <luo19930414@gmail.com>
2015-03-20 15:31:59 -0400

    Blocking wall and some refactors

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -11,26 +11,32 @@ public class AvatarController : MonoBehaviour {
 	public Camera main_camera;
 
 	private bool is_walking;
+	private bool can_walk;
+	private Vector3 walking_origin;
 	private Vector3 walking_dest;
 
 	private bool is_rotating;
 	private Quaternion rotate_dest;
 
+	private Rigidbody rigidbody;
+
 	// Use this for initialization
 	void Start () {
 		is_walking = false;
+		can_walk = true;
+		rigidbody = GetComponent<Rigidbody>();
+		walking_origin = transform.position;
 		player_camera.enabled = true;
 		main_camera.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (is_walking) {
-			if (transform.position == walking_dest) {
-				is_walking = false;
-				return;
-			}
-			transform.position = Vector3.MoveTowards(transform.position, walking_dest, walking_speed * Time.deltaTime);
+//		Debug.Log ("Actual position: " + transform.position);
+//		Debug.Log ("Theoretical position: " + transform.position);
+		if (is_walking && Vector3.Distance(transform.position, walking_dest) <= walking_speed * Time.deltaTime) {
+			Stop ();
+			walking_origin = walking_dest;
 			return;
 		}
 		if (is_rotating) {
@@ -42,20 +48,17 @@ public class AvatarController : MonoBehaviour {
 			return;
 		}
 
-		if (!is_walking && Input.GetKey ("space")) {
+		if (can_walk && !is_walking && Input.GetKey ("space")) {
+			Debug.Log ("Walk!!!!!!!!!!!!!!!!");
 			is_walking = true;
-			walking_dest = transform.position + transform.rotation * Vector3.right * grid_size;
+			walking_dest = walking_origin + transform.rotation * Vector3.right * grid_size;
+			rigidbody.velocity = transform.rotation * Vector3.right * walking_speed;
 			return;
 		}
 
-		if (!is_rotating && Input.GetKeyDown (KeyCode.RightArrow)) {
-			is_rotating = true;
-			rotate_dest = transform.rotation * Quaternion.Euler (0, 90, 0);
+		if (checkAndTurn (KeyCode.RightArrow, 90)) {
 			return;
-		}
-		if (!is_rotating && Input.GetKeyDown (KeyCode.LeftArrow)) {
-			is_rotating = true;
-			rotate_dest = transform.rotation * Quaternion.Euler (0, -90, 0);
+		} else if (checkAndTurn (KeyCode.LeftArrow, -90)) {
 			return;
 		}
 
@@ -66,5 +69,31 @@ public class AvatarController : MonoBehaviour {
 			main_camera.enabled = false;
 			player_camera.enabled = true;
 		}
+	}
+
+	// When bump into walls
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Bang!!!!!!!!!!!!!!!!");
+		Stop ();
+		can_walk = false;
+		transform.position = walking_origin;
+	}
+
+	void OnTriggerExit(Collider other) {
+		can_walk = true;
+	}
+
+	private void Stop() {
+		is_walking = false;
+		rigidbody.velocity = new Vector3(0, 0, 0);
+	}
+
+	private bool checkAndTurn(KeyCode keycode, int rotation) {
+		if (!is_rotating && !is_walking && Input.GetKeyDown (keycode)) {
+			is_rotating = true;
+			rotate_dest = transform.rotation * Quaternion.Euler (0, rotation, 0);
+			return true;
+		}
+		return false;
 	}
 }
