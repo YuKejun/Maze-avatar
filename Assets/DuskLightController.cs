f7615285f6edfcd67a85a8b33463130eb94ba841
YuKejun <yukejun@umich.edu>
2015-03-22 19:56:48 -0400

    light adjustment at start; add tutorial scene (uncomplete)

---
+++ Assets/DuskLightController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuskLightController : MonoBehaviour {
+
+	public float duration = 1.0f;
+	public float targetIntensity = 0.5f;
+	public Light light;
+	
+	// Use this for initialization
+	void Start () {
+		light.intensity = 0;
+		StartCoroutine (InitializeLight (duration));
+	}
+
+	IEnumerator InitializeLight(float duration) {
+		while (Mathf.Abs(light.intensity - targetIntensity) > 0.05f) {
+			light.intensity = Mathf.MoveTowards (light.intensity, targetIntensity, targetIntensity / duration * Time.deltaTime);
+			yield return null;
+		}
+		print("done");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
