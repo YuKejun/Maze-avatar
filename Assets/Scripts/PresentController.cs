98c13a5a43e9e21bca9bb07e177dddc0625e9c57
shiyonghu <sharonhsy93@gmail.com>
2015-03-22 23:24:53 -0400

    add spinning present

---
+++ Assets/Scripts/PresentController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PresentController : MonoBehaviour
+{
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				transform.Rotate (new Vector3 (0, 50, 0) * Time.deltaTime);
+		}
+		void OnTriggerEnter (Collider other)
+		{
+				Destroy (gameObject);
+				//add congratulation
+		}
+}
+
