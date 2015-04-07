baf9d70de679e8aa63d87d355d77cfa6bf982ed9
shiyonghu <sharonhsy93@gmail.com>
2015-04-06 22:21:55 -0400

    remove coroutine

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -26,43 +26,43 @@ public class TutorialScript : MonoBehaviour
 		// Update is called once per frame
 		void FixedUpdate ()
 		{
-				StartCoroutine (Tutor ());
+				Tutor ();
 		}
 
-		IEnumerator Tutor ()
+		void Tutor ()
 		{
 				if (finishBEV == true) {
 						if (Input.GetKey ("3")) {
-								yield return new WaitForSeconds (waitTime);
+//								yield return new WaitForSeconds (waitTime);
 								tutorial.text = "Congratulation! You have finished the tutorial!\nThe game will start now.";
-								yield return new WaitForSeconds (waitTime);
+//								yield return new WaitForSeconds (waitTime);
 								Application.LoadLevel (InGameMenuScript.levelToLoad);
 						}
 				} else if (finishTurnRight == true) {
 						//BEV now
 						if (Input.GetKey ("3")) {
-								yield return new WaitForSeconds (waitTime);
+//								yield return new WaitForSeconds (waitTime);
 								finishBEV = true;
 								tutorial.text = "Spread fingers again to switch back.";
 						}
 				} else if (finishTurnLeft == true) {
 						//turn right now
 						if (Input.GetKey ("right")) {
-								yield return new WaitForSeconds (waitTime);
+//								yield return new WaitForSeconds (waitTime);
 								finishTurnRight = true;
 								tutorial.text = "Spread fingers to switch to birds'-eye-view.";
 						}
 				} else if (finishWalk == true) {
 						//turn left now
 						if (Input.GetKey ("left")) {
-								yield return new WaitForSeconds (waitTime);
+//								yield return new WaitForSeconds (waitTime);
 								finishTurnLeft = true;
 								tutorial.text = "Wave right to turn right.";
 						}
 				} else {
 						//walk now
 						if (Input.GetKey ("space")) {
-								yield return new WaitForSeconds (waitTime);
+//								yield return new WaitForSeconds (waitTime);
 								finishWalk = true;
 								tutorial.text = "Wave left to turn left.";
 						}
