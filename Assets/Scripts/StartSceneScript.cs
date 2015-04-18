d325a95299d5b2a84e4d222adb06f8ef54801b06
WeijiaSun <waikasun@umich.edu>
2015-04-18 17:17:40 -0400

    highlight implemented

--- Assets/Scripts/StartSceneScript.cs
+++ Assets/Scripts/StartSceneScript.cs
@@ -1,26 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StartSceneScript : MonoBehaviour {
 
+	public Button levelOneButton;
+	public Button levelTwoButton;
+	public Button birdEyeOn;
+	public Button birdEyeOff;
+
 	public void levelOneButtonClicked()
 	{
 		InGameMenuScript.levelToLoad = 1;
+		levelOneButton.GetComponent<Image> ().color = Color.white;
+		levelTwoButton.GetComponent<Image> ().color = Color.black;
 	}
 
 	public void levelTwoButtonClicked()
 	{
 		InGameMenuScript.levelToLoad = 2;
+		levelOneButton.GetComponent<Image> ().color = Color.black;
+		levelTwoButton.GetComponent<Image> ().color = Color.white;
 	}
 
 	public void birdEyeOnButtonClicked()
 	{
 		CharactorController.can_switch = true;
+		birdEyeOn.GetComponent<Image> ().color = Color.white;
+		birdEyeOff.GetComponent<Image> ().color = Color.black;
 	}
 
 	public void birdEyeOffButtonClicked()
 	{
 		CharactorController.can_switch = false;
+		birdEyeOn.GetComponent<Image> ().color = Color.black;
+		birdEyeOff.GetComponent<Image> ().color = Color.white;
 	}
 
 	public void startButtonClicked()
