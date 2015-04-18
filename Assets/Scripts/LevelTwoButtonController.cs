42816b3d9e3a7f6c1e9c85c9519da2dae62e1ec4
YuKejun <yukejun@umich.edu>
2015-04-18 17:20:46 -0400

    coming soon part1 script

---
+++ Assets/Scripts/LevelTwoButtonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelTwoButtonController : MonoBehaviour {
+
+	public void levelTwoPointerEnter()
+	{
+		Text label = gameObject.GetComponent<Text> ();
+		label.text = "Coming Soon!";
+		label.fontSize = 13;
+	}
+	
+	public void levelTwoPointerExit() {
+		Text label = gameObject.GetComponent<Text> ();
+		label.text = "Level 2";
+		label.fontSize = 20;
+	}
+
+}
